0ad4ce94b7f6e0e39737695982147cdc67cc8a36
8840911852 <mauryavanshiymanish@gmail.com>
2025-06-26 15:37:30 +0530

    Create password_matching.py

---
+++ password_matching.py
@@ -0,0 +1,8 @@
+username1=("manish","himanhsu","dog")
+password1=(8840911852,55637,63738)
+username=input("username:")
+password=int(input("pasword:"))
+if username in username and password in password1:
+    print("yes you are valid.")
+else:
+    print("invalid username or password.")
